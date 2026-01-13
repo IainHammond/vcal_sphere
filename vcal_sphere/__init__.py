2b0befa8500e3daef8687ac247c2ecb7706b7f0d
IainHammond <dr.iain.hammond@gmail.com>
2026-01-13 14:30:45 +1100

    updated version

--- vcal_sphere/__init__.py
+++ vcal_sphere/__init__.py
@@ -1,6 +1,10 @@
-__version__ = "0.0.1"
-
 from . import calib
 from . import preproc
 from . import postproc
 from . import utils
+
+def __getattr__(name: str):
+    if name == '__version__':
+        from importlib.metadata import version
+        return version('vip_hci')
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
