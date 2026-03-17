3f21d1d2e94c044b18470d7585803c183b1dcc10
IainHammond <dr.iain.hammond@gmail.com>
2026-03-17 15:21:43 +0100

    fix for logic error during check for reference cube

--- src/vcal/postproc/postproc_IFS.py
+++ src/vcal/postproc/postproc_IFS.py
@@ -921,7 +921,7 @@ def postproc_IFS(params_postproc_name='VCAL_params_postproc_IFS.json',
 
 
         label_test_ori = label_test
-        if ref_cube_name is None or ref_cube_name != "":
+        if not ref_cube_name:
             ref_cube = None  # can't input ref_cube = [None] * nz to the ASDI functions
         ###################### 7. PCA-SADI full (single step) #########################
         if do_sadi_full and sadi_steps==1:
