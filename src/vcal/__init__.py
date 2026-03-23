6bb4b23711966ccf39b73736a3a25396d6b5e003
IainHammond <dr.iain.hammond@gmail.com>
2026-03-23 16:09:34 +0100

    PEP 562 and correct version printing

--- src/vcal/__init__.py
+++ src/vcal/__init__.py
@@ -1,10 +1,21 @@
-from . import calib
-from . import preproc
-from . import postproc
-from . import utils
+import importlib as _importlib
+
+_submodules = [
+    "calib",
+    "preproc",
+    "postproc",
+    "utils",
+]
+
 
 def __getattr__(name: str):
-    if name == '__version__':
+    if name in _submodules:
+        return _importlib.import_module(f".{name}", __name__)
+    if name == "__version__":
         from importlib.metadata import version
-        return version('vip_hci')
+        return version("vcal")
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+
+def __dir__():
+    return _submodules + ["__version__"]
