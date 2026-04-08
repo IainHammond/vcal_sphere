ebdd8f79a9f6ffbefd40b94031bdf316f66823c0
IainHammond <dr.iain.hammond@gmail.com>
2026-04-08 10:36:30 +0200

    step 10 fix for ValueError: setting an array element with a sequence

--- src/vcal/preproc/preproc_IRDIS.py
+++ src/vcal/preproc/preproc_IRDIS.py
@@ -2390,7 +2390,7 @@ def preproc_IRDIS(params_preproc_name='VCAL_params_preproc_IRDIS.json',
                 fluxes[ff] = open_fits(
                     outpath+final_fluxname+"{}.fits".format(filt))[0]
                 lbdas_tmp[ff] = open_fits(
-                    outpath+final_fwhmname+"{}.fits".format(filt))
+                    outpath+final_fwhmname+"{}.fits".format(filt))[0]
                 derot_angles = open_fits(
                     outpath+final_anglename+"{}.fits".format(filt))
 
